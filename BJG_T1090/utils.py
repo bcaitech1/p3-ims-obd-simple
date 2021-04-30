fd458d1c81f03795d0750f7465e3d903ecc83f22
JaegyeongByun <71882533+JaegyeongByun@users.noreply.github.com>
2021-05-01 00:21:52 +0900

    utils.py mIoU 수정

--- BJG_T1090/utils.py
+++ BJG_T1090/utils.py
@@ -21,7 +21,7 @@ def seed_everything(random_seed=21):
     np.random.seed(random_seed)
     random.seed(random_seed)
 
-
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
 def _fast_hist(label_true, label_pred, n_class):
     mask = (label_true >= 0) & (label_true < n_class)
     hist = np.bincount(
@@ -29,26 +29,14 @@ def _fast_hist(label_true, label_pred, n_class):
         label_pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
     return hist
 
-
-def label_accuracy_score(label_trues, label_preds, n_class):
+def label_accuracy_score(hist):
     """Returns accuracy score evaluation result.
-      - overall accuracy
-      - mean accuracy
       - mean IU
-      - fwavacc
     """
-    hist = np.zeros((n_class, n_class))
-    for lt, lp in zip(label_trues, label_preds):
-        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
-    acc = np.diag(hist).sum() / hist.sum()
-    with np.errstate(divide='ignore', invalid='ignore'):
-        acc_cls = np.diag(hist) / hist.sum(axis=1)
-    acc_cls = np.nanmean(acc_cls)
     with np.errstate(divide='ignore', invalid='ignore'):
         iu = np.diag(hist) / (
             hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
         )
     mean_iu = np.nanmean(iu)
-    freq = hist.sum(axis=1) / hist.sum()
-    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-    return acc, acc_cls, mean_iu, fwavacc
+
+    return mean_iu
